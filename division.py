8b178819f1741106db147be341599b916e5c5601
subashdhanush <85820233+subashdhanush@users.noreply.github.com>
2024-06-04 14:45:30 +0530

    Add files via upload

---
+++ division.py
@@ -0,0 +1,7 @@
+num3=float(input("enter a number"))
+num4=float(input("enter a number"))
+if num4 == '0':
+    print("Enter the number to divide")
+else:    
+   divresult=num3 / num4
+   print(divresult)
