fc2924e1c7a2db76b42749f5dd0862c06903d4d6
subashdhanush <85820233+subashdhanush@users.noreply.github.com>
2024-06-10 17:39:18 +0530

    Add files via upload

---
+++ 24.hcf.py
@@ -0,0 +1,12 @@
+def hcf(x,y):
+    if x>y:
+        smaller=y
+    else:
+        smaller=x
+    for i in range(1,smaller+1):
+        if (x % i ==0) and (y % i ==0):
+            hcf=i
+    return hcf
+num1=int(input("enter a number"))
+num2=int(input("enter a number"))  
+print(hcf(num1,num2))                
